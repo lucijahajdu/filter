b762011ffd65953afd441911f03e713b3d3a4df4
lucijahajdu <lucija.hajdu@gmail.com>
2023-08-06 09:23:47 +0000

    The filter method.

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === "Paul");
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,14 @@ const students = [
     ]
   },
 ];
+
+//const candidates = students.filter(student => {
+  //let strongSkills = student.skills.filter(skill => skill.yrsExperience >=5);
+  //return strongSkills.length > 0;
+//});
+//console.log(candidates);
+
+const has5yearsexp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsexp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
